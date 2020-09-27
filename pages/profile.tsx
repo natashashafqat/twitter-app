5e23601db5cad641037c62e8607c1dd855c008e0
natashashafqat <natashashafqat@gmail.com>
2020-09-28 00:06:32 +0100

    feat(): loading users own tweets;

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavBarComponent from '../src/components/NavBarComponent';
-import ProfileComponent from '../src/components/ProfileComponent';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import getConfig from 'next/config';
+const { serverRuntimeConfig } = getConfig();
+import { TimelineTweet } from '../src/interfaces/TimelineTweet.interface';
 
-class Profile extends Component {
-  render() {
-    return (
+export function Profile({ timeline }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
     <div>
       <NavBarComponent />
-      <ProfileComponent />
-    </div>    
-    )
-  }
+      {timeline.map((tweet: TimelineTweet) => {
+        return (
+          <ul>
+            <li>{tweet.user.name}</li>
+            <li>{tweet.text}</li>
+            <li>Retweets: {tweet.retweet_count}</li>
+            <li>Favourites: {tweet.favorite_count}</li>
+          </ul>
+        )
+      })}
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const timelineUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=natashamulla93&count=10";
+  const timelineResult = await fetch(timelineUrl, {
+    headers: {
+      'Authorization': `Bearer ${serverRuntimeConfig.TWITTER_BEARER_TOKEN}`
+    }
+  });
+  const timeline: TimelineTweet[] = await timelineResult.json();
+  return {
+    props: {
+      timeline
+    }
+  };
 }
 
 export default Profile;
